eb834a0fb1767c5853d050d1c567151365dc5d70
ClaudioGuzman1 <89861372+ClaudioGuzman1@users.noreply.github.com>
2026-01-21 14:42:41 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,27 @@
-const CACHE_NAME = "pwa-cache-v1";
-const urlsToCache = [
+const CACHE = "offline-v1";
+
+const OFFLINE_ASSETS = [
   "/",
-  "/index.html"
+  "/index.html",
+  "/manifest.json",
+  "/icons/icon-192.png",
+  "/icons/icon-256.png",
+  "/icons/icon-512.png"
 ];
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(OFFLINE_ASSETS))
   );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+self.addEventListener("activate", e => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
